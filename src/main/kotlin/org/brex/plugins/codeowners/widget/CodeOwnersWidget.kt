0cf9e1b99079e2a112d72a49108d04cb0acc0920
Joseph Atkins-Turkish <jatkins-turkish@brex.com>
2021-05-18 13:46:36 -0700

    Handle single owner

--- src/main/kotlin/org/brex/plugins/codeowners/widget/CodeOwnersWidget.kt
+++ src/main/kotlin/org/brex/plugins/codeowners/widget/CodeOwnersWidget.kt
@@ -25,24 +25,35 @@ class CodeOwnersWidget(project: Project) : EditorBasedWidget(project), StatusBar
 
     override fun getTooltipText(): String? = "Click to show in CODEOWNERS file"
 
-    override fun getSelectedValue(): String? = "CODEOWNERS: ${makeOwnersDescription(codeOwners)}"
+    override fun getSelectedValue(): String? = "CODEOWNERS: ${makeOwnersDescription(getCodeOwners())}"
 
     override fun getClickConsumer(): Consumer<MouseEvent>? = null
 
     override fun getPresentation(): StatusBarWidget.WidgetPresentation? = this
 
     /** Return a popup listing all codeowners for a file */
-    override fun getPopupStep(): ListPopup {
-        return JBPopupFactory.getInstance().createListPopup(object : BaseListPopupStep<String>("All CODEOWNERS", codeOwners?.owners) {
-            override fun onChosen(selectedValue: String?, finalChoice: Boolean): PopupStep<*>? {
-                val codeownersFile = CodeOwners(project.basePath!!).codeownersFile()
-                if (codeownersFile != null) {
-                    OpenFileDescriptor(project, codeownersFile, codeOwnerRule?.lineNumber ?: 0, 0).navigate(true)
-                }
+    override fun getPopupStep(): ListPopup? {
+        val owners = getCodeOwners()
+        if (owners === null || owners.owners.size <= 1) {
+            // Not sure if there's a better place to handle clicking when there's
+            // only one owner.
+            goToOwner()
+            return null
+        }
 
+        return JBPopupFactory.getInstance().createListPopup(object : BaseListPopupStep<String>("All CODEOWNERS", owners.owners) {
+            override fun onChosen(selectedValue: String?, finalChoice: Boolean): PopupStep<*>? {
+                goToOwner()
                 return super.onChosen(selectedValue, finalChoice)
             }
         })
+    }
+
+    private fun goToOwner() {
+        val codeOwnersFile = codeOwnersService?.codeownersFile()
+        if (codeOwnersFile != null) {
+            OpenFileDescriptor(project, codeOwnersFile, codeOwnerRule?.lineNumber ?: 0, 0).navigate(true)
+        }
     }
 
     /** Reload CodeOwners if the current file has changed */
@@ -50,15 +61,14 @@ class CodeOwnersWidget(project: Project) : EditorBasedWidget(project), StatusBar
     private var codeOwnerRule: CodeOwnerRule? = null
     private val codeOwnersService: CodeOwners? = project.basePath?.let { CodeOwners(it) }
 
-    private val codeOwners: CodeOwnerRule?
-        get() {
-            val file = selectedFile ?: return null
-            if (selectedFile !== codeOwnerFile) {
-                codeOwnerFile = selectedFile
-                codeOwnerRule = codeOwnersFromFile(file)
-            }
-            return codeOwnerRule
+    private fun getCodeOwners(): CodeOwnerRule? {
+        val file = selectedFile ?: return null
+        if (selectedFile !== codeOwnerFile) {
+            codeOwnerFile = selectedFile
+            codeOwnerRule = codeOwnersFromFile(file)
         }
+        return codeOwnerRule
+    }
 
     /** Update status bar text when opening a new file */
     override fun fileOpened(source: FileEditorManager, file: VirtualFile) {
@@ -71,13 +81,13 @@ class CodeOwnersWidget(project: Project) : EditorBasedWidget(project), StatusBar
         myStatusBar.updateWidget(ID())
     }
 
-    /** Load codeowners from a file */
+    /** Load code owners from a file */
     private fun codeOwnersFromFile(file: VirtualFile): CodeOwnerRule? {
         return codeOwnersService?.getCodeowners(file.path)
     }
 }
 
-/** Describe a list of codeowners */
+/** Describe a list of code owners */
 fun makeOwnersDescription(codeOwners: CodeOwnerRule?): String {
     val owners = codeOwners?.owners ?: return "None"
     return owners.first() + when {
